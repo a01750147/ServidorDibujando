0661e467b23e0e7693de49076f426c0ef8490845
a01750147 <65176328+a01750147@users.noreply.github.com>
2021-09-22 16:22:42 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const bodyParser = require("body-parser");
+const path = require("path")
+const express = require('express');
+const sequelize = require('./util/database') 
+
+const app=express()
+
+app.use(express.static(path.join(__dirname,"public"))); 
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+let puerto=8080;
+// Establece vínculo entre la conexión del servidor y la BD
+sequelize.sync({force:false})
+    .then(resultado=>{
+        console.log("Conexión exitosa");
+        // Lanza el servidor para escuchar peticiones
+        app.listen(puerto, ()=>console.log("Servidor en línea en el puerto 8080")); 
+    })
+    .catch(error=>console.log(error));       
