921316511861eff74f13ca9066c17e6c05481efd
A01750185 <A01750185@itesm.mx>
2021-10-15 16:42:23 -0500

    Guardar donaciones a base de datos

---
+++ routes/donacion.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+const donacionController = require('../controllers/donacion');
+
+// Mini app 
+const router = express.Router()
+
+//crear perfil
+router.post('/NuevaDonacion',donacionController.postAgregarDonacion);
+
+
+
+module.exports = router; 
